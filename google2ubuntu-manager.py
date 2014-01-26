9904851a3a8d3b5c7aedf91bc0281ab962099d47
benoitfragit <benoitfranquet@gmail.com>
2014-01-26 17:44:40 +0100

    passer les lignes incorrectes

--- google2ubuntu-manager.py
+++ google2ubuntu-manager.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-  
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-  
 # use  the new PyGObject binding
 from gi.repository import Gtk
 from gi.repository import Notify
--- google2ubuntu.py
+++ google2ubuntu.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-  
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from subprocess import *
 from gi.repository import Gtk
 from gi.repository import Notify
