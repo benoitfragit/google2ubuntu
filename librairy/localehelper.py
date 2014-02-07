42f37827d8c7a21c1eccf04d7923d1dd88cf448d
Tectas <adrian.stoeckl@gmail.com>
2014-02-07 13:44:38 +0100

    Locale configuration helper class added

---
+++ librairy/localehelper.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+from os.path import expanduser
+import locale
+import os
+
+RELATIVE_LOCALE_CONFIG_PATH = '/.config/google2ubuntu/locale.conf'
+DEFAULT_LOCALE = 'en'
+
+class LocaleHelper:
+    def __init__(self, languageFolder = os.path.dirname(os.path.abspath(__file__))+'/../i18n/'):
+        systemLocale = locale.getlocale()
+        
+        self.__systemLocale = None
+        
+        if systemLocale is not None and len(systemLocale) > 0:
+            if systemLocale[0] is not None and len(systemLocale[0]) > 0:
+                self.__systemLocale = systemLocale[0].split('_')[0]
+        
+        self.__languageFolder = languageFolder
+        self.__localeConfPath = expanduser('~')+RELATIVE_LOCALE_CONFIG_PATH
+    
+    def __getSystemLocale(self):
+        lc = locale.getlocale()[0].split('_')[0]
+        if self.__checkIfLocalePresent(lc):
+            return lc
+        else:
+            return DEFAULT_LOCALE
+        
+    def __getLocaleConfigValue(self):
+        configFileHandle = None
+        localeConfig = None
+        try:
+            configFileHandle = open(self.__localeConfPath, 'r')
+            localeConfig = configFileHandle.readline().strip('\n')
+        except:
+            pass
+        finally:
+            if configFileHandle:
+                configFileHandle.close()
+        
+        if localeConfig is None or localeConfig == '':
+            return self.__getSystemLocale()
+        else:
+            if self.__checkIfLocalePresent(localeConfig):
+                return localeConfig
+            else:
+                return DEFAULT_LOCALE
+            
+    def __checkIfLocalePresent(self, lang):
+        return os.path.isdir(self.__languageFolder+lang) == True
+    
+    def getLocale(self):
+        return self.__getLocaleConfigValue()
